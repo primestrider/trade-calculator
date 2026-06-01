d79967c2a448783c8cea23e9eae1811d117aee39
primestrider <theprimestrider@gmail.com>
2026-06-01 22:48:14 +0700

    add zustand

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,7 @@
-import { useState } from "react";
-
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <div></div>
-      <h1 className="text-2xl font-bold">Test {count}</h1>
+      <div>Test</div>
     </>
   );
 }
