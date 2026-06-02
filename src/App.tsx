80214519be7b3d0f8a5510a5c66ed7eeb19247d0
Ricky Ng <theprimestrider@gmail.com>
2026-06-02 17:58:28 +0700

    add shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,29 @@
+import { useEffect } from "react";
+import { SelectTicker } from "./components/SelectTicker";
+import { Card, CardContent, CardHeader, CardTitle } from "./components/ui/card";
+import { Separator } from "./components/ui/separator";
+
 function App() {
+  useEffect(() => {
+    document.documentElement.classList.add("dark");
+  }, []);
+
   return (
-    <>
-      <div>Test</div>
-    </>
+    <main className="min-h-screen font-manrope p-3 md:p-5">
+      <Card className="mx-auto max-w-3xl ">
+        <CardHeader>
+          <CardTitle>
+            <h1 className="text-neutral-50 font-bold text-xl text-center">
+              Trade Calculator Setup
+            </h1>
+          </CardTitle>
+        </CardHeader>
+        <div className="px-3">
+          <Separator />
+        </div>
+        <CardContent>asd</CardContent>
+      </Card>
+    </main>
   );
 }
 
